73876749b0ae46b1d6a53242d6dbd35b24d2db0d
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-01-06 13:27:03 +0100

    visit each grid point

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,57 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn parse_line(s: &str) -> [i64; 4] {
+fn parse_line(s: &str) -> [i64; 5] {
     let mut nums = s
         .split([',', ':', '='])
         .filter_map(|s| s.parse::<i64>().ok());
 
-    [
-        nums.next().unwrap(),
-        nums.next().unwrap(),
-        nums.next().unwrap(),
-        nums.next().unwrap(),
-    ]
+    let x_sensor = nums.next().unwrap();
+    let y_sensor = nums.next().unwrap();
+    let x_beacon = nums.next().unwrap();
+    let y_beacon = nums.next().unwrap();
+    let dist_sensor_beacon = i64::abs(x_sensor - x_beacon) + i64::abs(y_sensor - y_beacon);
+
+    [x_sensor, y_sensor, x_beacon, y_beacon, dist_sensor_beacon]
 }
 
 fn main() {
     let file = File::open("15.txt").unwrap();
-    let mut nums = Vec::new();
-    for line in BufReader::new(file).lines() {
-        let line = line.unwrap();
+    let vectors = BufReader::new(file)
+        .lines()
+        .map(|l| parse_line(&l.unwrap()))
+        .collect::<Vec<_>>();
 
-        let coords = parse_line(&line);
-        println!("{:?}", coords);
+    let x_sensor_min = vectors.iter().map(|coord| coord[0]).min().unwrap();
+    let x_sensor_max = vectors.iter().map(|coord| coord[0]).max().unwrap();
+    let y_sensor_min = vectors.iter().map(|coord| coord[1]).min().unwrap();
+    let y_sensor_max = vectors.iter().map(|coord| coord[1]).max().unwrap();
 
-        nums.push(coords);
+    let x_beacon_min = vectors.iter().map(|coord| coord[2]).min().unwrap();
+    let x_beacon_max = vectors.iter().map(|coord| coord[2]).max().unwrap();
+    let y_beacon_min = vectors.iter().map(|coord| coord[3]).min().unwrap();
+    let y_beacon_max = vectors.iter().map(|coord| coord[3]).max().unwrap();
+
+    let x_min = i64::min(x_sensor_min, x_beacon_min);
+    let y_min = i64::min(y_sensor_min, y_beacon_min);
+    let x_max = i64::max(x_sensor_max, x_beacon_max);
+    let y_max = i64::max(y_sensor_max, y_beacon_max);
+    println!("({}, {}) ({}, {})", x_min, y_min, x_max, y_max);
+
+    let mut visited = 0u64;
+    let mut count = 0i64;
+    for y in y_min..=y_max {
+        for x in x_min..=x_max {
+            visited += 1;
+            for [x_sensor, y_sensor, _x_beacon, _y_beacon, dist_sensor_beacon] in &vectors {
+                let dist_sensor_point = i64::abs(x_sensor - x) + i64::abs(y_sensor - y);
+
+                if &dist_sensor_point <= dist_sensor_beacon {
+                    count += 1;
+                    break;
+                }
+            }
+        }
     }
+    println!("{}/{}", count, visited);
 }
